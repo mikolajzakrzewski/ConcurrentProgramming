1fddbece7c758664140cccb0d5005da20fbca18d
Wojciech Florczak <florczak.wojciechv@gmail.com>
2024-04-02 21:55:21 +0200

    Created Ball class and added sample code.

---
+++ Logic/Ball.cs
@@ -0,0 +1,20 @@
+﻿namespace Logic
+{
+    public class Ball
+    {
+        int positionX;
+        int positionY;
+        int radius;
+        double velocityX;
+        double velocityY;
+
+        public Ball(int x, int y, int radius, double startVelocityX, double startVelocityY)
+        {
+            this.positionX = x;
+            this.positionY = y;
+            this.radius = radius;
+            this.velocityX = startVelocityX;
+            this.velocityY = startVelocityY;
+        }
+    }
+}
