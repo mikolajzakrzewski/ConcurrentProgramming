f4dd57e091b84533b726525139f30b78d04477c6
Wojciech Florczak <florczak.wojciechv@gmail.com>
2024-04-03 17:15:25 +0200

    Impelmented Model layer.

---
+++ Model/Model.cs
@@ -0,0 +1,33 @@
+﻿using Logic;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    internal class Model : ModelAPI
+    {
+        private readonly Table table;
+        public override int Width => table.Width;
+
+        public override int Height => table.Height;
+
+        public override void CreateBalls(int number, int radius)
+        {
+            table.CreateBalls(number, radius);
+        }
+
+        public override void Start()
+        {
+            table.Start();
+        }
+
+        public override void ResetTable()
+        {
+            table.ResetTable();
+        }
+    }
+}
---
+++ Model/ModelAPI.cs
@@ -0,0 +1,11 @@
+﻿namespace Model
+{
+    public abstract class ModelAPI
+    {
+        public abstract int Width { get; }
+        public abstract int Height { get; }
+        public abstract void CreateBalls(int number, int radius);
+        public abstract void Start();
+        public abstract void ResetTable();
+    }
+}
