df8a77f0bbf1cef3b9a3e89a3402c96e9764d8f9
Wojciech Florczak <florczak.wojciechv@gmail.com>
2024-04-13 17:33:24 +0200

    Added ModelTest.

---
+++ ModelTest/ModelTest.cs
@@ -0,0 +1,38 @@
+using Model;
+
+namespace ModelTest
+{
+    [TestClass]
+    public class ModelTest
+    {
+        [TestMethod]
+        public void TestCreateBalls()
+        {
+            ModelAPI model;
+            model = ModelAPI.Instance();
+            int numberOfBalls = 5;
+            int radius = 10;
+
+            model.CreateBalls(numberOfBalls, radius);
+
+            Assert.AreEqual(numberOfBalls, model.Balls.Count);
+            foreach (var ball in model.Balls)
+            {
+                Assert.AreEqual(radius, ball.Radius);
+            }
+        }
+
+        [TestMethod]
+        public void TestResetTable() 
+        {
+            ModelAPI model;
+            model = ModelAPI.Instance();
+
+            model.CreateBalls(5, 10);
+
+            model.ResetTable();
+
+            Assert.AreEqual(0, model.Balls.Count);
+        }
+    }
+}
