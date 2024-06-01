31cff10254db017a9b61db9e8c4498812d14b121
mikolajzakrzewski <mikizakr@gmail.com>
2024-06-01 18:01:23 +0200

    BallDto class code cleanup

--- Data/BallDto.cs
+++ Data/BallDto.cs
@@ -1,24 +1,23 @@
 ﻿using System.Numerics;
 
-namespace Data
-{
-    internal class BallDto
-    {
-        public int BallId { get; }
-        public float PositionX { get; }
-        public float PositionY { get; }
-        public float VelocityX { get; }
-        public float VelocityY { get; }
-        public string Date {  get; }
+namespace Data;
 
-        public BallDto(int id, Vector2 position, Vector2 velocity, string date)
-        {
-            BallId = id;
-            PositionX = position.X;
-            PositionY = position.Y;
-            VelocityX = velocity.X;
-            VelocityY = velocity.Y;
-            Date = date;
-        }
+internal class BallDto
+{
+    public BallDto(int id, Vector2 position, Vector2 velocity, string date)
+    {
+        BallId = id;
+        PositionX = position.X;
+        PositionY = position.Y;
+        VelocityX = velocity.X;
+        VelocityY = velocity.Y;
+        Date = date;
     }
+
+    public int BallId { get; }
+    public float PositionX { get; }
+    public float PositionY { get; }
+    public float VelocityX { get; }
+    public float VelocityY { get; }
+    public string Date { get; }
 }
